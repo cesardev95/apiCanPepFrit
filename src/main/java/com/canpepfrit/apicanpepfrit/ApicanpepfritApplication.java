8cac0d3ca0690ca5c73b7701f1db8a844dab2c43
Cesar <cesarmpibizadev@gmail.com>
2026-08-24 11:58:50 +0200

    Sincronizando local-github

---
+++ src/main/java/com/canpepfrit/apicanpepfrit/ApicanpepfritApplication.java
@@ -0,0 +1,13 @@
+package com.canpepfrit.apicanpepfrit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicanpepfritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicanpepfritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canpepfrit/apicanpepfrit/ApicanpepfritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canpepfrit.apicanpepfrit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicanpepfritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
