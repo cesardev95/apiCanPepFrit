96f614f5a94bba890697da4bf34c554ea2189162
Cesar <cesarmpibizadev@gmail.com>
2026-08-24 19:09:10 +0200

    -Inserto datos de prueba en la tabla

---
+++ src/main/resources/db/migration/V3__Insert_Datatest.sql
@@ -0,0 +1,5 @@
+INSERT INTO db_canpepfrit.alojamientos(nombre, direccion, telefono) VALUES ('TEST1','TEST DIRECCION','TEST TELEFONO');
+INSERT INTO db_canpepfrit.alojamientos(nombre, direccion, telefono) VALUES ('TEST2','TEST DIRECCION','TEST TELEFONO');
+INSERT INTO db_canpepfrit.alojamientos(nombre, direccion, telefono) VALUES ('TEST3','TEST DIRECCION','TEST TELEFONO');
+INSERT INTO db_canpepfrit.alojamientos(nombre, direccion, telefono) VALUES ('TEST4','TEST DIRECCION','TEST TELEFONO');
+INSERT INTO db_canpepfrit.alojamientos(nombre, direccion, telefono) VALUES ('TEST5','TEST DIRECCION','TEST TELEFONO');
