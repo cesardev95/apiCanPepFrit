536cf2e813078c64d12cfa72c521ec34dba0e700
Cesar <cesarmpibizadev@gmail.com>
2026-08-24 18:51:44 +0200

    -Modificacion en el pom.xml para que las dependencias MapStruct y Lombok no interfieran entre sí -Creacion de un get en el controller del alojamiento

--- src/main/java/com/canpepfrit/apicanpepfrit/controllers/AlojamientoController.java
+++ src/main/java/com/canpepfrit/apicanpepfrit/controllers/AlojamientoController.java
@@ -1,11 +1,22 @@
 package com.canpepfrit.apicanpepfrit.controllers;
 
+import com.canpepfrit.apicanpepfrit.DTOS.AlojamientoDTO;
+import com.canpepfrit.apicanpepfrit.services.AlojamientoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/alojamientos")
 public class AlojamientoController {
+    @Autowired
+    private AlojamientoService service;
+    @GetMapping
+    public List<AlojamientoDTO> findAll(){
+        return  service.servAlojamientoList();
+    }
 
 }
