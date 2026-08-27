cbe305eaf9c3938b5b0ce830f0357ec4602665df
Cesar <cesarmpibizadev@gmail.com>
2026-08-27 12:14:43 +0200

    -Creación de las clases Servicio y Controller del modelo Cliente.

--- src/main/java/com/canpepfrit/apicanpepfrit/controllers/ClienteController.java
+++ src/main/java/com/canpepfrit/apicanpepfrit/controllers/ClienteController.java
@@ -1,9 +1,40 @@
 package com.canpepfrit.apicanpepfrit.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.canpepfrit.apicanpepfrit.DTOS.ClienteDTO;
+import com.canpepfrit.apicanpepfrit.services.ClienteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<ClienteDTO> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ClienteDTO findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+    @PostMapping
+    public ClienteDTO addCliente(@RequestBody ClienteDTO dto){
+       return service.addCliente(dto);
+    }
+    @PutMapping("/{id}")
+    public ClienteDTO modifCliente(@PathVariable Long id, ClienteDTO dto){
+        return service.modifCliente(id,dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteClient(@PathVariable Long id){
+        return service.deleteClient(id);
+    }
 }
--- src/main/java/com/canpepfrit/apicanpepfrit/services/ClienteService.java
+++ src/main/java/com/canpepfrit/apicanpepfrit/services/ClienteService.java
@@ -1,10 +1,15 @@
 package com.canpepfrit.apicanpepfrit.services;
 
+import com.canpepfrit.apicanpepfrit.DTOS.ClienteDTO;
+import com.canpepfrit.apicanpepfrit.DTOS.ClienteLightDTO;
 import com.canpepfrit.apicanpepfrit.mappers.ClienteMapper;
 import com.canpepfrit.apicanpepfrit.models.Alojamiento;
 import com.canpepfrit.apicanpepfrit.models.Cliente;
 import com.canpepfrit.apicanpepfrit.repositories.RepoCliente;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -14,6 +19,44 @@ public class ClienteService {
     public ClienteService(RepoCliente repositorio, ClienteMapper mapper) {
         this.repositorio = repositorio;
         this.mapper = mapper;
+    }
+
+    public List<ClienteDTO> findAll(){
+        return mapper.toDTOList(repositorio.findAll());
+    }
+
+    public ClienteDTO findById(Long id){
+        Optional<Cliente> exists = repositorio.findById(id);
+        if(exists.isPresent()){
+            Cliente c = exists.get();
+            return mapper.toDTO(c);
+        }else{
+            throw new RuntimeException("No se ha econtrado cliente con id: "+id);
+        }
+
+    }
+
+    public ClienteDTO addCliente(ClienteDTO dto){
+        Cliente c = mapper.toEntity(dto);
+        Cliente saved = repositorio.save(c);
+        return mapper.toDTO(saved);
+    }
+
+    public ClienteDTO modifCliente(Long id,ClienteDTO dto){
+        Optional<Cliente> exists = repositorio.findById(id);
+        if(exists.isPresent()){
+            Cliente c = exists.get();
+            Cliente saved = repositorio.save(c);
+            return mapper.toDTO(saved);
+        } else{
+            throw new RuntimeException("No se ha encontrado cliente con id: "+id);
+        }
+
+    }
+
+    public String deleteClient(Long id){
+        repositorio.deleteById(id);
+        return "Cliente con id("+id+") borrado correctamente";
     }
 
     public Cliente obtainReference(Long id) {
